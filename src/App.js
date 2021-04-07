c0e75f23519640afe19fd448d9fa34c75368ee8b
amirtabatabaei69 <45861382+amirtabatabaei69@users.noreply.github.com>
2021-04-07 22:15:23 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer";
+
 import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Navbar /> */}
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/projects" component={Projects} />
         </Switch>
-        {/* <Footer /> */}
       </Router>
     </div>
   );
